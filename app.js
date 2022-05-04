d8fee53cffc0fbbe73ca402b4cc7c1ed9b5d121c
Jorge Duran <jldb6962016@gmail.com>
2022-05-04 04:32:04 -0500

    Primera version portafolio

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var app = require('express')();
+var express =require('express');
+var app = express();
 var http = require('http').createServer(app);
 
 var port = process.env.PORT || 3000;
 
 //routing
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
-app.get('/contact', function(req, res){
-  res.sendFile(__dirname + '/contacto.html');
+app.get('/inicio', function(req, res){
+  res.sendFile(__dirname + '/public/inicio.html');
+});
+app.get('/acerca_de', function(req, res){
+  res.sendFile(__dirname + '/public/acerca_de.html');
+});
+app.get('/portafolio', function(req, res){
+  res.sendFile(__dirname + '/public/portafolio.html');
+});
+app.get('/contacto', function(req, res){
+  res.sendFile(__dirname + '/public/contacto.html');
 });
 
+app.use(express.static("public"))
 /*io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
